4d41f033288866a9c801bc4284a5a02e16e2ec4d
Julian Duque <julianduquej@gmail.com>
2021-03-25 14:01:41 -0500

    add db support

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -15,8 +23,27 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello Salesforce Devs from Express</h1>');
 });
 
-app.get('/api/sessions', (req, res) => {
-  res.json(JSON.parse(sessions));
+app.get('/api/sessions', async (req, res) => {
+  const { rows } = await db.query(`
+  SELECT json_build_object(
+    'id', t.id,
+    'name', t.name,
+    'description', t.description,
+    'room', t.room,
+    'dateTime', t."datetime",
+    'speakers', json_agg(json_build_object(
+      'id', s.id,
+      'name', s.name,
+      'bio', s.bio,
+      'pictureUrl', s.pictureurl
+      ))
+    ) AS session
+  FROM sessions t
+  INNER JOIN sessions_speakers ts ON ts.session_id = t.id
+  INNER JOIN speakers s ON ts.speaker_id = s.id
+  GROUP BY t.id
+  `);
+  res.json(rows);
 });
 
 app.listen(port, () => {
